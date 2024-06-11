288f05203f2aae08eedc8a092d9ec804ef4b2761
Md. Shariful Islam <freelancershariful10@gmail.com>
2024-06-11 16:59:09 +0600

    Prod API Url

--- src/redux/api/api.ts
+++ src/redux/api/api.ts
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const baseApi = createApi({
 	reducerPath: "baseApi",
-	baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:5000" }),
+	baseQuery: fetchBaseQuery({ baseUrl: "https://todo-apiserver.vercel.app" }),
 	tagTypes: ["todo"],
 
 	endpoints: (builder) => ({
